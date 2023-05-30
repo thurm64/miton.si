9b5b78faa148181a8118305b440ccdf97eeac16b
thurm64 <63270342+thurm64@users.noreply.github.com>
2023-05-30 16:46:40 -0400

    sd
    ss

--- index.php
+++ index.php
@@ -13,14 +13,7 @@ echo file_get_contents("templates/nav_template.html");
   <h2>o kama pona a!</h2>
     <p>toki a! mi jan Jami. ni li lipu mi pi toki pona! </p>
     <p>tenpo ni la, ijo lili li lon lipu ni. tenpo kama la, ijo sin li kama. tan ni la, o lukin lon tenpo kama lili!</p>
-  <h2>o toki tawa mi!</h2>
-  <p>sina wile toki tawa mi la sina ken lon ni: <b>jami@miton.si</b></p>
-  <p>
-    <ul>
-      <li>sina wile e ijo sin lon lipu ni? o toki!</li>
-      <li>sina wile la mi pana e <abbr title="shortened link">linja lili</abbr> lon lipu ni tawa sina? o toki!</li>
-    </ul>
-  </p>
+
     
 
 </main>
---
+++ mi.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<?php
+echo file_get_contents("templates/header_info.html");
+?>
+
+<body>
+<?php
+echo file_get_contents("templates/nav_template.html");
+?>
+
+
+<main>
+  <h2>mi</h2>
+  <h3>jan Jami</h3>
+    <p>toki a! mi jan Jami. mi lon e lipu ni.
+      <br>mi tonsi. mi olin e ale. mi tan ma Mewika. tenpo sike <abbr title="luka luka luka tu wan (18)">LLLTW</abbr> la mi lon.
+      <br>mi la, musi <abbr title="Tetris">Tesi</abbr> en musi <abbr title="Chess">Sesi</abbr> en musi <abbr title="Minecraft">Manka</abbr> li musi.
+    </p>
+    <p></p>
+  <h2>o toki tawa mi!</h2>
+  <p>sina wile toki tawa mi la sina ken:</p>
+  <ul>
+    <li>kepeken <abbr title="Email">ilo pi lipu tawa</abbr>: <b>jami@miton.si</b></li>
+    <li>kepeken ilo <abbr title="Discord">Siko</abbr>: <b>thurm#1337</b></p></li>
+  </ul>
+  <p>kin la, mi kepeken ilo ni:</p>
+  <ul>
+    <li>ilo <abbr title="Youtube">Jutu</abbr>: <b><a target="_blank" href="https://www.youtube.com/@janJami-gh8rq">jan Jami</a></b></li>
+    <li>kepeken ilo <abbr title="Discord">Siko</abbr>: <b>thurm#1337</b></p></li>
+  </ul>
+</main>
+</body>
+</html>
