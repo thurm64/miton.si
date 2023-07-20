a7d5a90fe4911d78295aec2a6d33b45965fcce03
thurm64 <63270342+thurm64@users.noreply.github.com>
2023-07-20 18:02:55 -0400

    Update poki-kalama.php

--- poki-kalama.php
+++ poki-kalama.php
@@ -9,7 +9,7 @@ echo file_get_contents("templates/header_info.html");
 echo file_get_contents("templates/nav_template.html");
 ?>
 
-
+ 
 <main>
 <h1>poki kalama</h1>
 <video controls="" autoplay="" name="media"><source src="https://snowy-mountain-59653.pktriot.net/kalama.ogg" type="audio/mpeg"></video>
