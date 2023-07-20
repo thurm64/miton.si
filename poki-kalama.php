eb504204178655abf1a015d8e2584faed1dc8fb5
thurm64 <63270342+thurm64@users.noreply.github.com>
2023-07-20 18:05:42 -0400

    Update poki-kalama.php

--- poki-kalama.php
+++ poki-kalama.php
@@ -12,6 +12,7 @@ echo file_get_contents("templates/nav_template.html");
  
 <main>
 <h1>poki kalama</h1>
+<h2>o kute e poki kalama lon lipu ni! wawa</h2>
 <video controls="" autoplay="" name="media"><source src="https://snowy-mountain-59653.pktriot.net/kalama.ogg" type="audio/mpeg"></video>
 </main>
 </body>
