a5ecb10531044b57a99372c906497b76e5dc7510
thurm64 <63270342+thurm64@users.noreply.github.com>
2023-07-20 18:00:49 -0400

    Create poki-kalama.php

---
+++ poki-kalama.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<?php
+echo file_get_contents("templates/header_info.html");
+?>
+
+<body>
+<?php
+echo file_get_contents("templates/nav_template.html");
+?>
+
+
+<main>
+<h1>poki kalama</h1>
+<video controls="" autoplay="" name="media"><source src="https://snowy-mountain-59653.pktriot.net/kalama.ogg" type="audio/mpeg"></video>
+</main>
+</body>
+</html>
