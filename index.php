72be2a9aa4a0c1b561941f08ebe6c36dacf20797
thurm64 <63270342+thurm64@users.noreply.github.com>
2023-09-24 14:33:31 -0400

    ge

--- index.php
+++ index.php
@@ -13,18 +13,9 @@ echo file_get_contents("templates/nav_template.html");
   <h2>o kama pona a!</h2>
     <p>toki a! mi jan Jami. ni li lipu mi pi toki pona! </p>
     <p>tenpo ni la, ijo lili li lon lipu ni. tenpo kama la, ijo sin li kama. tan ni la, o lukin lon tenpo kama lili!</p>
-    <div id="mante" class="webring">
+
     <link rel="stylesheet" href="https://miton.si/mantewebringcss.css">
 
-        <div class="webring-inner">
-            <span class="webring-prev">&lt;- previous</span>
-            <span class="webring-info">This site is part of the mante webring<br>
-                    <a href="javascript:void(0)" onclick="randomSite()">random</a> | 
-                    <a href="https://the-blob-system.neocities.org/mante_webring/webring">index</a> | 
-                    <a href="https://garlic.garden/onionring/">what is this?</a></span>
-
-            <span class="webring-next">next -&gt;</span></td>
-        </div>
         <script type="text/javascript" src="https://the-blob-system.neocities.org/mante_webring/onionring-variables.js"></script>
         <script type="text/javascript" src="https://the-blob-system.neocities.org/mante_webring/onionring-widget.js"></script>
         </div>
