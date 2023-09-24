3572948220e7621fdcc264d74ec44a7130f54a8a
thurm64 <63270342+thurm64@users.noreply.github.com>
2023-09-24 14:34:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,9 @@ echo file_get_contents("templates/nav_template.html");
   <h2>o kama pona a!</h2>
     <p>toki a! mi jan Jami. ni li lipu mi pi toki pona! </p>
     <p>tenpo ni la, ijo lili li lon lipu ni. tenpo kama la, ijo sin li kama. tan ni la, o lukin lon tenpo kama lili!</p>
+    <link rel="stylesheet" href="https://miton.si/templates/mantewebringcss.css">
+    <div id="mante" class="webring"></div>
 
-    <link rel="stylesheet" href="https://miton.si/mantewebringcss.css">
 
         <script type="text/javascript" src="https://the-blob-system.neocities.org/mante_webring/onionring-variables.js"></script>
         <script type="text/javascript" src="https://the-blob-system.neocities.org/mante_webring/onionring-widget.js"></script>
