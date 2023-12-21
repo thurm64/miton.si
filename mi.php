a53a62705b0fb3a0ce06fe13fe487b31c19b7833
thurm64 <63270342+thurm64@users.noreply.github.com>
2023-12-21 16:28:15 -0500

    slight formatting fixes

--- mi.php
+++ mi.php
@@ -15,9 +15,9 @@ echo file_get_contents("templates/nav_template.html");
   # mi
 ### sina seme?
 toki! mi jan Jami!  nimi ante mi li ken "thurm"/"thurm64".  
-mi tonsi (any/all, ona) li olin e jan pi kule ale  
-mi tan ma Mewika li lon ma tenpo EST  
-mi lon tenpo sike luka luka luka tu tu (19)
+mi tonsi (any/all, ona) li olin e jan pi kule ale.  
+mi tan ma Mewika li lon ma tenpo EST.  
+mi lon tenpo sike luka luka luka tu tu. (19)
 ### ijo seme li pona tawa sina?
 #### musi ilo
 - musi Manka
@@ -29,8 +29,10 @@ mi lon tenpo sike luka luka luka tu tu (19)
 - musi supa nimi (Scrabble)
 #### pali
 *toki ilo:* mi kepeken toki ilo lon tenpo mute. tenpo poka la, mi kepeken ilo Junisi (Unity) tawa ijo pi musi Wijasa.  
+  
 *kalama musi:* mi pali e kalama musi pi nasin ni: ilo musi pi tenpo pini suli li ken ala kalama kepeken kalama mute. mi kepeken nasin pi ilo musi ni. tenpo ni la mi pali mute ala e ona, taso mi wile pali la mi ni.  kin la, mi kalama musi uta.   
-*toki pona:* tenpo mute la, mi ante e ijo musi tawa toki pona. mi toki pona taso lon tenpo mute. mi ni lon ilo Siko lon musi Wijasa kin.     
+  
+*toki pona:* ilo Siko en musi Wijasa la mi toki pona lon tenpo mute a! tenpo mute la, mi ante e ijo musi tawa toki pona.     
 mi jan lawa lon kulupu [*ma pona pi toki pona*](https://discord.gg/mapona). o kama!
 ### sina seme lon lipu ante?
 sina wile toki tawa mi lon ilo ante la o lukin e ni ale:
@@ -56,10 +58,11 @@ I'm 19 years old.
 - Scrabble
 #### projects/hobbies
 *programming:* I do a lot of programming. Recently, I've spent a lot of time making things for VRChat using Unity.
-*music:* I like to make chiptune music in the style of retro video game consoles. Right now I haven't done it as much, but if I have some inspiration I make more. I also like to sing. 
-
-*toki pona:* I like to speak the constructed language *toki pona* on Discord and VRChat with my friends! I also translate lots of stuff to toki pona.
-I am a moderator at [*ma pona pi toki pona*](https://discord.gg/mapona). Come and say hi!
+  
+*music:* I like to make chiptune music in the style of retro video game consoles. Right now I haven't done it as much, but if I have some inspiration I'll make more. I also like to sing.  
+  
+*toki pona:* I like to speak the constructed language *toki pona* on Discord and VRChat with my friends! I also translate lots of stuff to toki pona.  
+I am also a moderator at [*ma pona pi toki pona*](https://discord.gg/mapona). Come and say hi!  
 ### what are your socials?
 If you want to reach out, here are some of the other places you can find me:
 - email `jami@miton.si`
